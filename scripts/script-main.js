be356363b7aab21734697292fadcd0e8f408ac20
TimZZZ19 <53802591+TimZZZ19@users.noreply.github.com>
2022-06-20 23:45:44 -0500

    added responsive design

--- scripts/script-main.js
+++ scripts/script-main.js
@@ -1,78 +1,78 @@
 // Back to top on refreshing
 window.onbeforeunload = function () {
-  window.scrollTo(0, 0);
-};
+  window.scrollTo(0, 0)
+}
 
 // *************************
 // COMMENT SECTION SMOOTH SCROLL
 // *************************
-const navBar = document.querySelector(".nav-bar");
-const footerNav = document.querySelector(".footer-navigation");
+const navBar = document.querySelector('.nav-bar')
+const footerNav = document.querySelector('.footer-navigation')
 
 const smoothScroll = (sectionRect) => {
   window.scrollTo({
     left: sectionRect.left + window.scrollX,
     top: sectionRect.top + window.scrollY - 100,
-    behavior: "smooth",
-  });
-};
+    behavior: 'smooth',
+  })
+}
 
 const goToSection = (e) => {
-  if (!e.target.classList.contains("section-tab")) return;
-  e.preventDefault();
+  if (!e.target.classList.contains('section-tab')) return
+  e.preventDefault()
 
-  const id = e.target.getAttribute("href");
-  const section = document.querySelector(id);
-  const sectionRect = section.getBoundingClientRect();
+  const id = e.target.getAttribute('href')
+  const section = document.querySelector(id)
+  const sectionRect = section.getBoundingClientRect()
 
-  smoothScroll(sectionRect);
-};
+  smoothScroll(sectionRect)
+}
 
-navBar.addEventListener("click", (e) => goToSection(e));
+navBar.addEventListener('click', (e) => goToSection(e))
 
-footerNav.addEventListener("click", (e) => goToSection(e));
+footerNav.addEventListener('click', (e) => goToSection(e))
 
 // COMMENT Scroll to hero on clicking the header brand
-const headerBrand = document.querySelector(".header-brand");
+const headerBrand = document.querySelector('.header-brand')
 
-headerBrand.addEventListener("click", () => {
-  const section = document.getElementById("hero");
-  const sectionRect = section.getBoundingClientRect();
+headerBrand.addEventListener('click', () => {
+  const section = document.getElementById('hero')
+  const sectionRect = section.getBoundingClientRect()
 
-  smoothScroll(sectionRect);
-});
+  smoothScroll(sectionRect)
+})
 
 // *************************
 // SECTION INDICATOR
 // *************************
-let activeSectionTab;
-const sections = document.querySelectorAll(".section");
+let activeSectionTab
+const sections = document.querySelectorAll('.section')
 
 const updateActiveTab = (tab) => {
-  activeSectionTab = document.querySelector(".active-tab");
-  activeSectionTab.classList.remove("active-tab");
-  tab.classList.add("active-tab");
-};
+  activeSectionTab = document.querySelector('.active-tab')
+  activeSectionTab.classList.remove('active-tab')
+  tab.classList.add('active-tab')
+}
 
 const options = {
   root: null,
-  rootMargin: "-49% 0px",
+  rootMargin: '-49% 0px',
   threshold: 0,
-};
+}
 
 const progressObserver = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
-    if (!entry.isIntersecting) return;
-    const id = entry.target.getAttribute("id");
-    const currentTab = document.getElementById(`${id}-tab`);
+    if (!entry.isIntersecting) return
+    const id = entry.target.getAttribute('id')
+    const currentTab = document.getElementById(`${id}-tab`)
 
-    updateActiveTab(currentTab);
-  });
-}, options);
+    updateActiveTab(currentTab)
+  })
+}, options)
 
 sections.forEach((section) => {
-  progressObserver.observe(section);
-});
+  progressObserver.observe(section)
+})
 
 // *************************
 // SECTION REVEALING EFFECT
@@ -80,19 +80,19 @@ sections.forEach((section) => {
 
 const revealSection = function (entries, observer) {
   entries.forEach((entry) => {
-    if (!entry.isIntersecting) return;
-    entry.target.classList.remove("section--hidden");
-    observer.unobserve(entry.target);
-  });
-};
+    if (!entry.isIntersecting) return
+    entry.target.classList.remove('section--hidden')
+    observer.unobserve(entry.target)
+  })
+}
 
 const sectionRevealObserver = new IntersectionObserver(revealSection, {
   root: null,
-  rootMargin: "0px 0px -25% 0px",
+  rootMargin: '0px 0px -30% 0px',
   threshold: 0,
-});
+})
 
 sections.forEach(function (section) {
-  sectionRevealObserver.observe(section);
-  section.classList.add("section--hidden");
-});
+  sectionRevealObserver.observe(section)
+  section.classList.add('section--hidden')
+})
