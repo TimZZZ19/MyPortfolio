2a670dced940f8a9631d1434d34bb8d622beda79
TimZ <tiomsity@gmail.com>
2022-08-31 02:43:09 -0500

    removed a few useless projects

--- scripts/script-slideshow.js
+++ scripts/script-slideshow.js
@@ -28,9 +28,9 @@ Slide.activate(
 /* PROJECT2 COMMENT */
 let counter2, timer2, manualNav2;
 const projectOneImgArr2 = [
-  "./images/WeatherApp/WeatherApp (1).PNG",
-  "./images/WeatherApp/WeatherApp (2).PNG",
-  "./images/WeatherApp/WeatherApp (3).PNG",
+  "./images/FoodApp2/FoodApp2 (1).png",
+  "./images/FoodApp2/FoodApp2 (2).png",
+  "./images/FoodApp2/FoodApp2 (3).png",
 ];
 
 Slide.activate(
@@ -40,18 +40,18 @@ Slide.activate(
   projectOneImgArr2,
   "2",
   "even",
-  "Weather App",
-  "A weather app.",
-  "React",
-  "https://mellifluous-pothos-355545.netlify.app/",
-  "https://github.com/TimZZZ19/WeatherApp"
+  "FoodApp2",
+  "A fictional online food-ordering App.<br> Managed Frontend data with Context API. <br> Used Firebase Realtime Database for storing App and user data.",
+  "React, Firebase",
+  "https://cute-cuchufli-1f8bd6.netlify.app/",
+  "https://github.com/TimZZZ19/FoodOrderApp2"
 );
 
 /* PROJECT3 COMMENT */
 let counter3, timer3, manualNav3;
 const projectOneImgArr3 = [
-  "./images/TodoList/TodoList (1).png",
-  "./images/TodoList/TodoList (2).png",
+  "./images/CodingGuru/CodingGuru (1).PNG",
+  "./images/CodingGuru/CodingGuru (2).PNG",
 ];
 
 Slide.activate(
@@ -61,74 +61,10 @@ Slide.activate(
   projectOneImgArr3,
   "3",
   "odd",
-  "Todo List",
-  "A Todo list.",
-  "React",
-  "https://zingy-liger-c5e0ae.netlify.app/",
-  "https://github.com/TimZZZ19/TodoList"
-);
-
-/* PROJECT4 COMMENT */
-let counter4, timer4, manualNav4;
-const projectOneImgArr4 = [
-  "./images/FoodApp2/FoodApp2 (1).png",
-  "./images/FoodApp2/FoodApp2 (2).png",
-  "./images/FoodApp2/FoodApp2 (3).png",
-];
-
-Slide.activate(
-  counter4,
-  timer4,
-  manualNav4,
-  projectOneImgArr4,
-  "4",
-  "even",
-  "FoodApp2",
-  "A fictional online food-ordering App.",
-  "React, Firebase Realtime Database",
-  "https://cute-cuchufli-1f8bd6.netlify.app/",
-  "https://github.com/TimZZZ19/FoodOrderApp2"
-);
-
-/* PROJECT5 COMMENT */
-let counter5, timer5, manualNav5;
-const projectOneImgArr5 = [
-  "./images/CodingGuru/CodingGuru (1).PNG",
-  "./images/CodingGuru/CodingGuru (2).PNG",
-];
-
-Slide.activate(
-  counter5,
-  timer5,
-  manualNav5,
-  projectOneImgArr5,
-  "5",
-  "odd",
   "Coding Guru",
-  "Landing page for a fictional coding bootcamp",
+  "Landing page for a fictional coding bootcamp. <br> Micmicked Coding Dojo's landing page. <br> My first time using a CSS framework.",
   "HTML, Tailwind, JavaScript",
   "https://jazzy-lollipop-ed99d3.netlify.app/",
-  "https://github.com/TimZZZ19/CodingGuru"
-);
-
-/* PROJECT5 COMMENT */
-let counter6, timer6, manualNav6;
-const projectOneImgArr6 = [
-  "./images/MovieList/MovieList (1).PNG",
-  "./images/MovieList/MovieList (2).PNG",
-];
-
-Slide.activate(
-  counter6,
-  timer6,
-  manualNav6,
-  projectOneImgArr6,
-  "6",
-  "even",
-  "MyMovieList",
-  "A simple app for recording movies that I want to watch",
-  "HTML, Bootstrap, JavaScript",
-  "https://delicate-sprite-cdb0cb.netlify.app/",
-  "https://github.com/TimZZZ19/MovieList",
+  "https://github.com/TimZZZ19/CodingGuru",
   false
 );
